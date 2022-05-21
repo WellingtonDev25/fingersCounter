6343bf62dee4022c70e5a8d54071581fbfb2a58d
WellingtonDev25 <56238012+WellingtonDev25@users.noreply.github.com>
2022-05-21 13:14:35 -0300

    alteraçao para gravaçao da aula

--- hands.py
+++ hands.py
@@ -1,14 +1,14 @@
 import cv2
 import mediapipe as mp
 
-cap = cv2.VideoCapture(1)
+video = cv2.VideoCapture(1)
 
 hands = mp.solutions.hands
 Hands = hands.Hands(max_num_hands=1)
 mpDwaw = mp.solutions.drawing_utils
 
 while True:
-    success, img = cap.read()
+    success, img = video.read()
     frameRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     results = Hands.process(frameRGB)
     handPoints = results.multi_hand_landmarks
@@ -20,7 +20,7 @@ while True:
             #podemos enumerar esses pontos da seguinte forma
             for id, cord in enumerate(points.landmark):
                 cx, cy = int(cord.x * w), int(cord.y * h)
-                cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                #cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                 pontos.append((cx,cy))
 
             dedos = [8,12,16,20]
@@ -32,7 +32,9 @@ while True:
                    if pontos[x][1] < pontos[x-2][1]:
                        contador +=1
 
-            print(contador)
+            cv2.rectangle(img, (80, 10), (200,110), (255, 0, 0), -1)
+            cv2.putText(img,str(contador),(100,100),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),5)
+            #print(contador)
 
     cv2.imshow('Imagem',img)
     cv2.waitKey(1)
